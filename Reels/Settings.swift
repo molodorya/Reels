fa16110608b1f01485c743fc95ec552515c3566e
Nikita Molodorya <nmolodorya@gmail.coom>
2022-10-19 12:48:58 +0300

    update

--- Reels/Settings.swift
+++ Reels/Settings.swift
@@ -9,21 +9,21 @@ import UIKit
 
 class Settings: UIViewController {
 
+    @IBOutlet weak var close: UIBarButtonItem!
+    
+    @IBOutlet weak var getPremium: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Settings"
+        getPremium.layer.cornerRadius = 15
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
     }
-    */
-
+    
 }
