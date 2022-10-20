aae22dc374b6e53ce1e5faef3c3b016c462d884a
Nikita Molodorya <nmolodorya@gmail.coom>
2022-10-21 00:53:13 +0300

    update

---
+++ Reels/Subscribe/BuySubscribe.swift
@@ -0,0 +1,24 @@
+//
+//  BuySubscribe.swift
+//  Reels
+//
+//  Created by Nikita Molodorya on 21.10.2022.
+//
+
+import UIKit
+
+class BuySubscribe: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+  
+
+}
