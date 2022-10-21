a5b21c98a28ea4c51d6a875e1927995f18b22097
Nikita Molodorya <nmolodorya@gmail.coom>
2022-10-21 23:00:31 +0300

    update

--- Reels/Main.swift
+++ Reels/Main.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class Main: UIViewController {
     
@@ -14,9 +15,13 @@ class Main: UIViewController {
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var collectionView: UICollectionView!
     
+   
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
+     
+            
         
         collectionView.delegate = self
         collectionView.dataSource = self
@@ -30,6 +35,7 @@ class Main: UIViewController {
 class TrendingsCollectionCell: UICollectionViewCell {
     
     
+    @IBOutlet weak var displayCell: UIView!
     @IBOutlet weak var nameVideo: UILabel!
     
     override class func awakeFromNib() {
@@ -52,9 +58,50 @@ extension Main: UICollectionViewDelegate, UICollectionViewDataSource {
         cell.nameVideo.text = "Video №\(indexPath.row)"
         cell.layer.cornerRadius = 25
         
+       // Повторяет видео после того как оно закончилось
+        func loopVideo(videoPlayer: AVPlayer) {
+          NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil, queue: nil) { notification in
+            videoPlayer.seek(to: .zero)
+            videoPlayer.play()
+          }
+        }
+        
+        switch indexPath.row {
+            
+            case 0:
+            let videoURL = URL(string: "https://www.iphones.ru/wp-content/uploads/2022/10/IMG_2520.mp4?_=3")
+            let player = AVPlayer(url: videoURL!)
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.frame = cell.displayCell.bounds
+            cell.displayCell.layer.addSublayer(playerLayer)
+            player.volume = 0
+          
+            loopVideo(videoPlayer: player)
+            player.play()
+            
+            case 1:
+            let videoURL = URL(string: "https://www.iphones.ru/wp-content/uploads/2022/10/IMG_2856.mp4?_=4")
+            let player = AVPlayer(url: videoURL!)
+            let playerLayer = AVPlayerLayer(player: player)
+
+            playerLayer.frame = cell.displayCell.bounds
+            cell.displayCell.layer.addSublayer(playerLayer)
+            player.volume = 0
+            
+            loopVideo(videoPlayer: player)
+            player.play()
+        default:
+            break
+        }
+       
+        
+        
+      
+        
         return cell
     }
     
     
 }
+
 
--- Reels/Subscribe/BuySubscribe.swift
+++ Reels/Subscribe/BuySubscribe.swift
@@ -6,13 +6,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class BuySubscribe: UIViewController {
+    
+    
+    @IBOutlet weak var videoView: UIView!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let videoURL = URL(string: "https://www.iphones.ru/wp-content/uploads/2022/10/IMG_2520.mp4?_=3")
+        let player = AVPlayer(url: videoURL!)
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.videoView.bounds
+        self.videoView.layer.addSublayer(playerLayer)
+        player.play()
     }
     
     @IBAction func close(_ sender: UIBarButtonItem) {
