03921984b76f04c530894d1eff1b19ebd1390e6f
Nikita Molodorya <nmolodorya@gmail.coom>
2022-10-27 00:48:10 +0300

    update

--- Reels/TrimVideo/VideoMain.swift
+++ Reels/TrimVideo/VideoMain.swift
@@ -51,42 +51,39 @@ class VideoMain: UIViewController {
     
 
     
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
       
       super.viewDidLoad()
       loadViews()
-      // Do any additional setup after loading the view, typically from a nib.
+        // Выполните любую дополнительную настройку после загрузки представления, как правило, из пера.
     }
 
-    override func didReceiveMemoryWarning()
-    {
+    override func didReceiveMemoryWarning() {
       super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
+        // Удаляем любые ресурсы, которые можно воссоздать.
     }
     
-    //Loading Views
-    func loadViews()
-    {
-      //Whole layout view
+    //Загрузка представлений
+    func loadViews() {
+        //Просмотр всего макета
       layoutContainer.layer.borderWidth = 1.0
       layoutContainer.layer.borderColor = UIColor.white.cgColor
       
       selectButton.layer.cornerRadius = 5.0
       cropButton.layer.cornerRadius   = 5.0
       
-      //Hiding buttons and view on load
+        //Скрываем кнопки и вид при загрузке
       cropButton.isHidden         = true
       startView.isHidden          = true
       endView.isHidden            = true
       frameContainerView.isHidden = true
       
-      //Style for startTime
+        //Стиль для startTime
       startTimeText.layer.cornerRadius = 5.0
       startTimeText.layer.borderWidth  = 1.0
       startTimeText.layer.borderColor  = UIColor.white.cgColor
       
-      //Style for endTime
+        //Стиль для endTime
       endTimeText.layer.cornerRadius = 5.0
       endTimeText.layer.borderWidth  = 1.0
       endTimeText.layer.borderColor  = UIColor.white.cgColor
@@ -99,14 +96,13 @@ class VideoMain: UIViewController {
       player = AVPlayer()
 
       
-      //Allocating NsCahe for temp storage
+        //Выделение NsCahe для временного хранилища
       cache = NSCache()
     }
     
-    //Action for select Video
-    @IBAction func selectVideoUrl(_ sender: Any)
-    {
-      //Selecting Video type
+    //Действие для выбора видео
+    @IBAction func selectVideoUrl(_ sender: Any) {
+        //Выбор типа видео
       let myImagePickerController        = UIImagePickerController()
       myImagePickerController.sourceType = .photoLibrary
       myImagePickerController.mediaTypes = [(kUTTypeMovie) as String]
@@ -115,9 +111,8 @@ class VideoMain: UIViewController {
       present(myImagePickerController, animated: true, completion: {  })
     }
     
-    //Action for crop video
-    @IBAction func cropVideo(_ sender: Any)
-    {
+    //Действие для обрезки видео
+    @IBAction func cropVideo(_ sender: Any) {
       let start = Float(startTimeText.text!)
       let end   = Float(endTimeText.text!)
       cropVideo(sourceURL1: url, startTime: start!, endTime: end!)
@@ -125,10 +120,9 @@ class VideoMain: UIViewController {
 
   }
 
-  //Subclass of VideoMainViewController
-  extension VideoMain: UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate
-  {
-    //Delegate method of image picker
+//Подкласс VideoMainViewController
+  extension VideoMain: UIImagePickerControllerDelegate,UINavigationControllerDelegate,UITextFieldDelegate {
+      //Делегировать метод выбора изображения
       func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
           picker.dismiss(animated: true, completion: nil)
           
@@ -156,17 +150,15 @@ class VideoMain: UIViewController {
           player.play()
       }
       
-    func viewAfterVideoIsPicked()
-    {
-      //Rmoving player if alredy exists
-      if(playerLayer != nil)
-      {
+    func viewAfterVideoIsPicked() {
+        //Перемещение игрока, если он уже существует
+      if(playerLayer != nil) {
         playerLayer.removeFromSuperlayer()
       }
       
       createImageFrames()
       
-      //unhide buttons and view after video selection
+        //показать кнопки и просмотреть после выбора видео
       cropButton.isHidden         = false
       startView.isHidden          = false
       endView.isHidden            = false
@@ -179,9 +171,8 @@ class VideoMain: UIViewController {
       createRangeSlider()
     }
     
-    //Tap action on video player
-      @objc func tapOnVideoLayer(tap: UITapGestureRecognizer)
-    {
+      //Нажмите действие в видеоплеере
+      @objc func tapOnVideoLayer(tap: UITapGestureRecognizer) {
       if isPlaying
       {
         player.play()
@@ -196,9 +187,8 @@ class VideoMain: UIViewController {
 
     
     //MARK: CreatingFrameImages
-    func createImageFrames()
-    {
-      //creating assets
+    func createImageFrames() {
+        //создание ассетов
       let assetImgGenerate : AVAssetImageGenerator    = AVAssetImageGenerator(asset: asset)
       assetImgGenerate.appliesPreferredTrackTransform = true
       assetImgGenerate.requestedTimeToleranceAfter    = CMTime.zero;
@@ -214,9 +204,8 @@ class VideoMain: UIViewController {
       var startTime = 1
       var startXPosition:CGFloat = 0.0
       
-      //loop for 6 number of frames
-      for _ in 0...5
-      {
+        // цикл на 6 кадров
+      for _ in 0...5 {
         
         let imageButton = UIButton()
         let xPositionForEach = CGFloat(imageFrameView.frame.width)/6
@@ -241,10 +230,9 @@ class VideoMain: UIViewController {
       
     }
     
-    //Create range slider
-    func createRangeSlider()
-    {
-      //Remove slider if already present
+      //Создаем ползунок диапазона
+    func createRangeSlider() {
+        //Удаляем ползунок, если он уже есть
       let subViews = frameContainerView.subviews
       for subview in subViews{
         if subview.tag == 1000 {
@@ -256,7 +244,7 @@ class VideoMain: UIViewController {
       frameContainerView.addSubview(rangeSlider)
       rangeSlider.tag = 1000
       
-      //Range slider action
+        //Действие ползунка диапазона
       rangeSlider.addTarget(self, action: #selector(VideoMain.rangeSliderValueChanged(_:)), for: .valueChanged)
       
       let time = DispatchTime.now() + Double(Int64(NSEC_PER_SEC)) / Double(NSEC_PER_SEC)
@@ -271,8 +259,7 @@ class VideoMain: UIViewController {
       @objc func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
       player.pause()
       
-      if(isSliderEnd == true)
-      {
+      if(isSliderEnd == true) {
         rangeSlider.minimumValue = 0.0
         rangeSlider.maximumValue = Double(thumbtimeSeconds)
         
@@ -285,13 +272,10 @@ class VideoMain: UIViewController {
       endTimeText.text   = "\(rangeSlider.upperValue)"
       
       //print(rangeSlider.lowerLayerSelected)
-      if(rangeSlider.lowerLayerSelected)
-      {
+      if(rangeSlider.lowerLayerSelected) {
         seekVideo(toPos: CGFloat(rangeSlider.lowerValue))
 
-      }
-      else
-      {
+      } else {
         seekVideo(toPos: CGFloat(rangeSlider.upperValue))
         
       }
@@ -302,29 +286,26 @@ class VideoMain: UIViewController {
     
     //MARK: TextField Delegates
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
-                   replacementString string: String) -> Bool
-    {
+                   replacementString string: String) -> Bool {
       let maxLength     = 3
       let currentString = startTimeText.text! as NSString
       let newString     = currentString.replacingCharacters(in: range, with: string) as NSString
       return newString.length <= maxLength
     }
     
-    //Seek video when slide
+      //Поиск видео при слайде
     func seekVideo(toPos pos: CGFloat) {
       videoPlaybackPosition = pos
       let time: CMTime = CMTimeMakeWithSeconds(Float64(videoPlaybackPosition), preferredTimescale: player.currentTime().timescale)
       player.seek(to: time, toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero)
       
-      if(pos == CGFloat(thumbtimeSeconds))
-      {
+      if(pos == CGFloat(thumbtimeSeconds)) {
       player.pause()
       }
     }
     
-    //Trim Video Function
-    func cropVideo(sourceURL1: NSURL, startTime:Float, endTime:Float)
-    {
+     //Функция обрезки видео
+    func cropVideo(sourceURL1: NSURL, startTime:Float, endTime:Float) {
       let manager                 = FileManager.default
       
       guard let documentDirectory = try? manager.url(for: .documentDirectory,
@@ -334,8 +315,7 @@ class VideoMain: UIViewController {
       guard let mediaType         = "mp4" as? String else {return}
       guard (sourceURL1 as? NSURL) != nil else {return}
       
-      if mediaType == kUTTypeMovie as String || mediaType == "mp4" as String
-      {
+      if mediaType == kUTTypeMovie as String || mediaType == "mp4" as String {
         let length = Float(asset.duration.value) / Float(asset.duration.timescale)
         //print("video length: \(length) seconds")
         
@@ -347,11 +327,11 @@ class VideoMain: UIViewController {
           try manager.createDirectory(at: outputURL, withIntermediateDirectories: true, attributes: nil)
           //let name = hostent.newName()
           outputURL = outputURL.appendingPathComponent("1.mp4")
-        }catch let error {
+        } catch let error {
           print(error)
         }
         
-        //Remove existing file
+          //удаляем существующий файл
         _ = try? manager.removeItem(at: outputURL)
         
         guard let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetHighestQuality) else {return}
@@ -377,7 +357,7 @@ class VideoMain: UIViewController {
           default: break
     }}}}
     
-    //Save Video to Photos Library
+      // Сохранить видео в библиотеке фотографий
     func saveToCameraRoll(URL: NSURL!) {
         PHPhotoLibrary.shared().performChanges({
         PHAssetChangeRequest.creationRequestForAssetFromVideo(atFileURL: URL as URL)
